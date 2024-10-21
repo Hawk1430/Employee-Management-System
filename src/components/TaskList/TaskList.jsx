e807db2ea8a9cc65aad22997cafa9166bcf0a7c7
Prashant Raj <pr2125116@gmail.com>
2024-10-21 12:56:29 +0530

    first commit

---
+++ src/components/TaskList/TaskList.jsx
@@ -0,0 +1,66 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const TaskList = () => {
+  return (
+    <div
+      id="tasklist"
+      className="h-[55%] flex overflow-x-auto items-center justify-start flex-nowrap gap-5 w-full py-5  mt-10"
+    >
+      <div className="h-full flex-shrink-0 w-[300px] p-5 bg-red-400 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded-sm">High</h3>
+          <h4 className="text-sm"> 20 feb 2024</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Make a youtube video</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, debitis
+          aut quam deleniti neque dolorum fuga cum. Eveniet, officiis. Assumenda
+          quidem aperiam voluptates dicta voluptatibus quam id, recusandae
+          dolore ratione?
+        </p>
+      </div>
+      <div className="h-full flex-shrink-0 w-[300px] p-5 bg-green-400 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded-sm">High</h3>
+          <h4 className="text-sm"> 20 feb 2024</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Make a youtube video</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, debitis
+          aut quam deleniti neque dolorum fuga cum. Eveniet, officiis. Assumenda
+          quidem aperiam voluptates dicta voluptatibus quam id, recusandae
+          dolore ratione?
+        </p>
+      </div>
+      <div className="h-full flex-shrink-0 w-[300px] p-5 bg-blue-400 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded-sm">High</h3>
+          <h4 className="text-sm"> 20 feb 2024</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Make a youtube video</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, debitis
+          aut quam deleniti neque dolorum fuga cum. Eveniet, officiis. Assumenda
+          quidem aperiam voluptates dicta voluptatibus quam id, recusandae
+          dolore ratione?
+        </p>
+      </div>
+      <div className="h-full flex-shrink-0 w-[300px] p-5 bg-yellow-400 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded-sm">High</h3>
+          <h4 className="text-sm"> 20 feb 2024</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Make a youtube video</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, debitis
+          aut quam deleniti neque dolorum fuga cum. Eveniet, officiis. Assumenda
+          quidem aperiam voluptates dicta voluptatibus quam id, recusandae
+          dolore ratione?
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default TaskList;
---
+++ src/components/other/Header.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="flex items-end justify-between">
+      <h1 className="text-2xl                   ">
+        Hello <br /> <span className="text-3xl font-semibold">Prashant 👋</span>
+      </h1>
+      <button className="bg-red-600 text-lg font-medium text-white px-3 py-2 rounded-sm">
+        Log Out
+      </button>
+    </div>
+  );
+};
+
+export default Header;
