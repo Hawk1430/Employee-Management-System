e7d7dc24802784c05a889025760c4afea2bdeb53
Prashant Raj <pr2125116@gmail.com>
2024-10-21 12:58:21 +0530

    made UI of taskLsit

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -59,6 +59,32 @@ const TaskList = () => {
           dolore ratione?
         </p>
       </div>
+      <div className="h-full flex-shrink-0 w-[300px] p-5 bg-red-400 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded-sm">High</h3>
+          <h4 className="text-sm"> 20 feb 2024</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Make a youtube video</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, debitis
+          aut quam deleniti neque dolorum fuga cum. Eveniet, officiis. Assumenda
+          quidem aperiam voluptates dicta voluptatibus quam id, recusandae
+          dolore ratione?
+        </p>
+      </div>
+      <div className="h-full flex-shrink-0 w-[300px] p-5 bg-green-400 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded-sm">High</h3>
+          <h4 className="text-sm"> 20 feb 2024</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Make a youtube video</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, debitis
+          aut quam deleniti neque dolorum fuga cum. Eveniet, officiis. Assumenda
+          quidem aperiam voluptates dicta voluptatibus quam id, recusandae
+          dolore ratione?
+        </p>
+      </div>
     </div>
   );
 };
