0ea6fc125bc986911ac3555009d5e38ec8309e29
Prashant Raj <pr2125116@gmail.com>
2024-10-23 13:25:53 +0530

    Made the tasks of employee appear on admin dashboard

--- src/components/TaskList/NewTask.jsx
+++ src/components/TaskList/NewTask.jsx
@@ -5,7 +5,9 @@ const NewTask = ({ task }) => {
   return (
     <div className="h-full flex-shrink-0 w-[300px] p-5 bg-blue-400 rounded-xl">
       <div className="flex justify-between items-center">
-        <h3 className="bg-red-600 text-sm px-3 py-1 rounded-sm">High</h3>
+        <h3 className="bg-red-600 text-sm px-3 py-1 rounded-sm">
+          {task.category}
+        </h3>
         <h4 className="text-sm"> {task.taskDate}</h4>
       </div>
       <h2 className="mt-5 text-2xl font-semibold">{task.taskTitle}</h2>
--- src/components/other/AllTask.jsx
+++ src/components/other/AllTask.jsx
@@ -1,42 +1,42 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "../../context/AuthProvider";
 
 const AllTask = () => {
+  const authData = useContext(AuthContext);
   return (
-    <div
-      id="allTask"
-      className="bg-[#1C1C1C] p-5 rounded mt-5 h-48 overflow-auto"
-    >
+    <div id="allTask" className="bg-[#1C1C1C] p-5 rounded mt-5 ">
       <div className="bg-red-400  py-2 px-4 flex justify-between mb-2 rounded">
-        <h2>Prashant</h2>
-        <h3>Make a UI Design</h3>
-        <h5>Status</h5>
+        <h2 className="text-lg font-medium  w-1/5">Employee Name</h2>
+        <h3 className="text-lg font-medium  w-1/5">New Task</h3>
+        <h5 className="text-lg font-medium  w-1/5">Active Task</h5>
+        <h5 className="text-lg font-medium  w-1/5">Completed </h5>
+        <h5 className="text-lg font-medium  w-1/5">Failed</h5>
       </div>
-      <div className="bg-yellow-400 py-2 px-4 flex justify-between mb-2 rounded">
-        <h2>Prashant</h2>
-        <h3>Make a UI Design</h3>
-        <h5>Status</h5>
-      </div>
-      <div className="bg-green-400 py-2 px-4 flex justify-between mb-2 rounded">
-        <h2>Prashant</h2>
-        <h3>Make a UI Design</h3>
-        <h5>Status</h5>
-      </div>
-      <div className="bg-purple-400 py-2 px-4 flex justify-between mb-2 rounded">
-        <h2>Prashant</h2>
-        <h3>Make a UI Design</h3>
-        <h5>Status</h5>
-      </div>
-      <div className="bg-red-400 py-2 px-4 flex justify-between mb-2 rounded">
-        <h2>Prashant</h2>
-        <h3>Make a UI Design</h3>
-        <h5>Status</h5>
-      </div>
-      <div className="bg-green-400 py-2 px-4 flex justify-between mb-2 rounded">
-        <h2>Prashant</h2>
-        <h3>Make a UI Design</h3>
-        <h5>Status</h5>
-      </div>
+      {authData.employees.map((elem, idx) => {
+        return (
+          <div
+            className="border-2 border-emerald-500 mb-2  py-2 px-4 flex justify-between rounded"
+            key={idx}
+          >
+            <h2 className="text-lg font-medium w-1/5 text-white">
+              {elem.firstName}
+            </h2>
+            <h3 className="text-lg font-medium w-1/5 text-blue-600">
+              {elem.newTasks}
+            </h3>
+            <h5 className="text-lg font-medium w-1/5 text-yellow-400">
+              {elem.activeTasks}
+            </h5>
+            <h5 className="text-lg font-medium w-1/5 text-green-600">
+              {elem.completedTasks}
+            </h5>
+            <h5 className="text-lg font-medium w-1/5 text-red-600">
+              {elem.failedTasks}
+            </h5>
+          </div>
+        );
+      })}
     </div>
   );
 };
